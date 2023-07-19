3ef43e5a8127dc1635f0258ff785096a2bd1460f
FurtunaGG <furtibiochem@gmail.com>
2023-07-19 06:17:21 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char lowCaseAlp;
+
+	for (lowCaseAlp = 'a'; lowCaseAlp <= 'z'; lowCaseAlp++)
+		_putchar(lowCaseAlp);
+
+	_putchar('\n');
+}
+
+
