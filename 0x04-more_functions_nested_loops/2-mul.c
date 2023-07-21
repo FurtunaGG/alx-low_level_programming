fcc94eefd6d875f22bf74f9592509489f086b9b6
FurtunaGG <furtibiochem@gmail.com>
2023-07-21 08:12:53 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiply 2 integers
+ * @a: first integer
+ * @b: second integer
+ * Return: int- multiplicattion of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
