d5efcd9835643a8a8721a514a165e8fd766ba16f
FurtunaGG <furtibiochem@gmail.com>
2023-07-21 08:39:42 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print numbers from 0-9, except 2 and 4
+ *
+ * return: void
+ */
+void print_most_numbers(void)
+{
+	char i;
+
+	for (i = '0';  i <= '9'; i++)
+	{
+		if (i == '2' || i == '4')
+			continue;
+		else
+			_putchar(i);
+	}
+	_putchar('\n');
+}
